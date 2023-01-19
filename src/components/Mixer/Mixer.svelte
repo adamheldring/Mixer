e174c58432a90e0360b708bb044bab4333ef8fe6
Adam Heldring <adam.heldring@svt.se>
2023-01-19 14:36:49 +0100

    add 3d effect to entire mixer unit

--- src/components/Mixer/Mixer.svelte
+++ src/components/Mixer/Mixer.svelte
@@ -23,27 +23,55 @@
 </script>
 
 <div class="flex justify-center p-5 mt-5">
-	<div class="mixer-container bg-gray-400">
-		{#if channels.length > 0}
-			<div transition:fade class="flex">
-				<div class="channels-container flex pl-1 py-1 bg-gray-400">
-					<div class="flex">
-						{#each channels as channel, channelIdx}
-							<ChannelStrip channelNr={channelIdx} {channel} />
-						{/each}
+	<div class="relative">
+		<div
+			class="mixer-bottom absolute bg-red-400 rounded-lg z-0 shadow-2xl"
+			style={`
+		top: 20px;
+		left: 6px;
+		width: 820px;
+		height: 625px;
+	`}
+		/>
+		<div class="mixer-top box-shadow-3d p-5 bg-gray-600 rounded-lg relative z-10">
+			<div class="mixer-container bg-gray-400 rounded overflow-hidden h-full">
+				{#if channels.length > 0}
+					<div transition:fade class="flex">
+						<div class="channels-container flex pl-1 py-1 bg-gray-400">
+							<div class="flex">
+								{#each channels as channel, channelIdx}
+									<ChannelStrip channelNr={channelIdx} {channel} />
+								{/each}
+							</div>
+						</div>
+						<MasterSection {handlePlay} {handleStop} {isPlaying} {masterGainNode} />
 					</div>
-				</div>
-				<MasterSection {handlePlay} {handleStop} {isPlaying} {masterGainNode} />
+				{:else}
+					<div class="h-full border-4 border-gray-400">
+						<LoaderDots />
+					</div>
+				{/if}
 			</div>
-		{:else}
-			<div class="h-full border-4 border-gray-400">
-				<LoaderDots />
-			</div>
-		{/if}
+		</div>
 	</div>
 </div>
 
 <style>
+	.box-shadow-3d {
+		--var-shadow-color: #9ca3af;
+		box-shadow: 1px 1px 0px var(--var-shadow-color), 1px 2px 0px var(--var-shadow-color),
+			1px 3px 0px var(--var-shadow-color), 1px 4px 0px var(--var-shadow-color),
+			2px 5px 0px var(--var-shadow-color), 2px 6px 0px var(--var-shadow-color),
+			2px 7px 0px var(--var-shadow-color), 2px 8px 0px var(--var-shadow-color),
+			3px 9px 0px var(--var-shadow-color), 3px 10px 0px var(--var-shadow-color),
+			3px 11px 0px var(--var-shadow-color), 3px 12px 0px var(--var-shadow-color),
+			4px 13px 0px var(--var-shadow-color), 4px 14px 0px var(--var-shadow-color),
+			4px 15px 0px var(--var-shadow-color), 4px 16px 0px var(--var-shadow-color),
+			5px 17px 0px var(--var-shadow-color), 5px 18px 0px var(--var-shadow-color),
+			5px 19px 0px var(--var-shadow-color), 5px 20px 0px var(--var-shadow-color),
+			6px 21px 0px var(--var-shadow-color), 6px 22px 0px var(--var-shadow-color),
+			6px 23px 0px var(--var-shadow-color), 6px 24px 0px var(--var-shadow-color);
+	}
 	.mixer-container {
 		min-width: 780px;
 		min-height: 581px;
