bf3d4db4ec874d38b5a8258b9d92f1a0bc9cacdb
Adam Heldring <adam.heldring@svt.se>
2023-01-21 19:39:56 +0100

    avoid needless scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,15 @@
 	import "../app.css";
 </script>
 
-<div class="bg-slate-200 min-h-screen">
-	<div class="top-margin lg:h-28 h-2" />
+<div class="bg-slate-200 lg:min-h-screen layout-wrapper">
+	<div class="top-margin lg:h-28" />
 	<slot />
 </div>
+
+<style>
+	@media (max-width: 850px) {
+		.layout-wrapper {
+			max-height: 400px;
+		}
+	}
+</style>
